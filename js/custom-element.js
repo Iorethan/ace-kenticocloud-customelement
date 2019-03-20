562047ddd722f03e63fbf2b0189d75d821fe6067
Milan Lund <hello@milanlund.com>
2019-03-20 09:13:31 +0100

    ftdr

--- js/custom-element.js
+++ js/custom-element.js
@@ -15,6 +15,7 @@
     };
 
     var setupAce = function (initValue, config) {
+        console.log(config);
         editor = ace.edit('editor');
         editor.setTheme('ace/theme/' + config.initTheme ? config.initTheme : 'monokai');
         editor.session.setMode('ace/mode/' + config.initMode ? config.initMode : 'javascript');
